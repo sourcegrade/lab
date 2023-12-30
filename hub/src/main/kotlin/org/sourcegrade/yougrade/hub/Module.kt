411552892a5e9b83201b9c4ee4d73f6073e5a3b9
Rdeisenroth <rdeisenroth@gmail.com>
2023-12-30 21:37:45 +0100

    lint

--- hub/src/main/kotlin/org/sourcegrade/yougrade/hub/Module.kt
+++ hub/src/main/kotlin/org/sourcegrade/yougrade/hub/Module.kt
@@ -10,7 +10,7 @@ fun Application.module() {
         schema {
             packages = listOf("org.sourcegrade.yougrade.hub")
             queries = listOf(
-                HelloWorldQuery()
+                HelloWorldQuery(),
             )
         }
     }
