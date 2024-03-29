ebe987dd1cc1bca5b2b7d49db102b08e01e877f7
Rdeisenroth <rdeisenroth@gmail.com>
2024-03-29 14:01:23 +0100

    grab courses from backend

--- common/ui/components/courses.tsx
+++ common/ui/components/courses.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { TypedDocumentNode, useQuery } from "@apollo/client";
 import gql from "graphql-tag";
-import { CourseDto } from "yougrade-hub/app/__generated__/graphql";
+import { CourseDto } from "lab-hub/app/__generated__/graphql";
 import withApollo, { client } from "@repo/ui/lib/withApollo";
-const query: TypedDocumentNode<{course: CourseDto}>
- = gql`
+import CardCarousel, {CardContent} from "./card-carousel";
+const query: TypedDocumentNode<{course: {fetchAll: CourseDto[]}}>
+= gql`
     query fetchAllCourses {
         course {
             fetchAll {
                 id
                 name
                 description
+                semesterStartYear
+                semesterType
             }
         }
     }
@@ -23,5 +26,15 @@ export default function Courses() {
     if(!data) {
         return <p>No data</p>;
     }
-    return <p>{JSON.stringify(data)}</p>;
+    const courses = data?.course.fetchAll;
+    console.log("courses", courses)
+    const cardCourses: CardContent[] = courses?.map((course) => ({
+        title: course.name,
+        description: course.description,
+        image: "https://www.pngkit.com/png/full/12-120436_open-book-icon-png.png",
+        openButtonEnabled: true,
+        settingsButtonEnabled: false,
+        openButtonHref: `/courses/${course.id}`,
+    })) || [];
+    return <CardCarousel items={cardCourses}/>;
 }
--- hub/ui/app/page.tsx
+++ hub/ui/app/page.tsx
@@ -1,25 +1,9 @@
-"use client";
 import Button from "@mui/material/Button";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import CardCarousel, {CardContent} from "@repo/ui/components/card-carousel";
 import Courses from "@repo/ui/components/courses";
 import _ from "lodash";
-import withApollo, { client } from "@repo/ui/lib/withApollo";
-import { getDataFromTree } from "@apollo/react-ssr";
-// import { gql } from "yougrade-hub/app/__generated__/gql";
-import gql from "graphql-tag";
-import { TypedDocumentNode, useQuery } from "@apollo/client";
-import { CourseDto } from "./__generated__/graphql";
-
-const demoCourses: CardContent[] = _.range(10).map((i) => ({
-    title: `Course ${i}`,
-    description: `Description ${i}`,
-    image: "https://www.pngkit.com/png/full/12-120436_open-book-icon-png.png",
-    openButtonEnabled: true,
-    settingsButtonEnabled: false,
-    openButtonHref: `/courses/${i}`,
-}));
 
 const demoAssignments: CardContent[] = _.range(10).map((i) => ({
     title: `Assignment ${i}`,
@@ -29,48 +13,22 @@ const demoAssignments: CardContent[] = _.range(10).map((i) => ({
     settingsButtonEnabled: false,
 }));
 
-const query: TypedDocumentNode<{course: CourseDto}>
- = gql`
-    query fetchAllCourses {
-        course {
-            fetchAll {
-                id
-                name
-                description
-                semesterStartYear
-                semesterType
-            }
-        }
-    }
-`;
-
 export default function Page() {
-    const { loading, data } = useQuery(query, {client});
-    var recentCourses:JSX.Element;
-    if (loading) {
-        recentCourses = <p>Loading...</p>;
-    } else {
-        recentCourses = <p>{JSON.stringify(data)}</p>;
-    }
-    console.log("data", data)
     return (
         <div>
             <h1>Dashboard</h1>
-            {/* {recentCourses} */}
-            <Courses></Courses>
-            <br/>
             <div className="flex justify-center space-x-6 p-3 rounded-md bg-slate-700">
                 <Button variant="contained" className="flex-col flex-grow" disabled>
-                    <AddCircleIcon className="!text-5xl" />
+                    <AddCircleIcon className="!text-5xl"/>
                     Create new Course
                 </Button>
                 <Button variant="contained" className="flex-col flex-grow" disabled>
-                    <FormatListBulletedIcon className="!text-5xl" />
+                    <FormatListBulletedIcon className="!text-5xl"/>
                     Show my Courses
                 </Button>
-            cCbb</div>
+            </div>
             <h2>Recent Courses</h2>
-            <CardCarousel items={demoCourses}/>
+            <Courses></Courses>
             <h2>Recent Assignments</h2>
             <CardCarousel items={demoAssignments}/>
         </div>
