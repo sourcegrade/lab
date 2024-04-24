252d93b72186a6d1234b7862ba6d0370e4d28794
Alexander Städing <alexanderstaeding@gmail.com>
2024-04-24 12:43:50 +0200

    Fix GradingRun

--- hub/src/main/kotlin/org/sourcegrade/lab/hub/db/GradingRuns.kt
+++ hub/src/main/kotlin/org/sourcegrade/lab/hub/db/GradingRuns.kt
@@ -4,8 +4,10 @@ import org.jetbrains.exposed.dao.id.UUIDTable
 
 internal object GradingRuns : UUIDTable("sgl_grading_runs") {
     val submissionId = reference("submission_id", Submissions)
-    val rubricId = reference("rubric_id", Rubrics)
     val gradedRubricId = reference("graded_rubric_id", GradedRubrics)
-    val minPoints = integer("minPoints")
-    val maxPoints = integer("maxPoints")
+    val rubricId = reference("rubric_id", Rubrics)
+    val minPoints = integer("min_points")
+    val maxPoints = integer("max_points")
+    val achievedMinPoints = integer("achieved_min_points")
+    val achievedMaxPoints = integer("achieved_max_points")
 }
--- hub/src/main/kotlin/org/sourcegrade/lab/hub/domain/GradingRun.kt
+++ hub/src/main/kotlin/org/sourcegrade/lab/hub/domain/GradingRun.kt
@@ -3,23 +3,19 @@ package org.sourcegrade.lab.hub.domain
 import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.id.EntityID
-import org.sourcegrade.lab.hub.db.GradedRubrics
 import org.sourcegrade.lab.hub.db.GradingRuns
 import java.util.UUID
 
-//data class GradingRun(
-//    override val id: UUID,
-//    val maxPoints: Int,
-//    val minPoints: Int,
-//    val rubric: ByteArray,
-//) : DomainEntity
-
 class GradingRun(id: EntityID<UUID>) : UUIDEntity(id) {
     val submission: Submission by Submission referencedOn GradingRuns.submissionId
-//    val achievedMinPoints: Int by GradingRuns.achievedMinPoints
-//    val achievedMaxPoints: Int by GradingRuns.achievedMaxPoints
-    val rubric: Rubric by Rubric referencedOn GradingRuns.rubricId
     val gradedRubric: GradedRubric by GradedRubric referencedOn GradingRuns.gradedRubricId
+
+    // duplicated data from GradedRubric to minimize joins for frequent queries
+    val rubric: Rubric by Rubric referencedOn GradingRuns.rubricId
+    val minPoints: Int by GradingRuns.minPoints
+    val maxPoints: Int by GradingRuns.maxPoints
+    val achievedMinPoints: Int by GradingRuns.achievedMinPoints
+    val achievedMaxPoints: Int by GradingRuns.achievedMaxPoints
 
     companion object : EntityClass<UUID, GradingRun>(GradingRuns)
 }
