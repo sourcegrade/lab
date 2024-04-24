2088d7a32296ddae3971b10528581805e7a7911a
Alexander Städing <alexanderstaeding@gmail.com>
2024-04-24 14:56:15 +0200

    Remove term comment

--- hub/src/main/kotlin/org/sourcegrade/lab/hub/domain/Term.kt
+++ hub/src/main/kotlin/org/sourcegrade/lab/hub/domain/Term.kt
@@ -6,13 +6,6 @@ import org.jetbrains.exposed.dao.id.EntityID
 import org.sourcegrade.lab.hub.db.Terms
 import java.util.UUID
 
-//data class Term(
-//    override val id: UUID,
-//    val name: String,
-//    val start: ZonedDateTime,
-//    val end: ZonedDateTime,
-//) : DomainEntity
-
 class Term(id: EntityID<UUID>) : UUIDEntity(id) {
     val name by Terms.name
     val start by Terms.start
